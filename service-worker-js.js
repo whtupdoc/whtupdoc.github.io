eedd52f1c0c6f70c533adf567750b3c536cb4e6c
whtupdoc <whtupdoc@gmail.com>
2024-10-02 20:36:07 -0400

    Add files via upload

---
+++ service-worker-js.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'wrvu-counter-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
